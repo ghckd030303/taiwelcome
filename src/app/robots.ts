7caec634710c6ab65542737425c9b73296f049f2
JengmoAn <ajm4232@gmail.com>
2026-03-05 10:36:13 +0900

    Fix lint error

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,22 +1,22 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/api/',
+        userAgent: "*",
+        allow: "/",
+        disallow: "/api/",
       },
       {
-        userAgent: 'Yeti', // 네이버 봇 전용 설정
-        allow: '/',
+        userAgent: "Yeti", // 네이버 봇 전용 설정
+        allow: "/",
       },
       {
-        userAgent: 'Googlebot', // 구글 봇 전용 설정
-        allow: '/',
+        userAgent: "Googlebot", // 구글 봇 전용 설정
+        allow: "/",
       },
     ],
-    sitemap: 'https://taiwelcome.com/sitemap.xml',
-  }
+    sitemap: "https://taiwelcome.vercel.app/sitemap.xml",
+  };
 }
